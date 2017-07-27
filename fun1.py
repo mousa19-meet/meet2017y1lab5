17079c56ec0d6c211229234db921ef2a06e64318
mousa19-meet <mousa19@meet.mit.edu>
2017-07-27 12:53:06 +0200

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,22 @@
+#def add_numbers():
+#    c = 0
+#    for number in range (1, 100 +1 ):
+#        c = c + number
+#    return c
+#answer = add_numbers()
+#print(answer)
+def add_number(start,end):
+    c = 0
+    for number in range(start , end + 1):
+        c = c + number
+    return c
+test1 = add_number(1,2)
+print(test1)
+test2 = add_number(1,100)
+print(test2)
+test3 = add_number(1000,5000)
+print(test3)
+test4 = add_number(333,777)
+print(test4)
+
+ 
---
+++ fun2.py
@@ -0,0 +1,9 @@
+star = input('hi give me anything')
+pop = input ("give another thing")
+def draw_1(n):
+    #star = input('give me a symbol ')
+    #pop = input ('give another one ' )
+    
+    print(star * n)
+    print(pop * n)
+    
